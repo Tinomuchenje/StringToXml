b58a9f3dda9a09510964ad94fad6f2726ecf3d1a
TinoJoel Muchenje <31763088+Tinomuchenje@users.noreply.github.com>
2022-04-28 13:34:10 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj2 = serializer.Deserialize<Message>(xml);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+[XmlRoot("Status")]
+public class Status{
+
+    [XmlElement("transactionId")]
+    public string TransactionId { get; set; }
+
+    [XmlElement("messageId")]
+    public string MessageId { get; set; }
+    
+    [XmlElement("successIndicator")]
+    public string SuccessIndicator { get; set; }
+
+    [XmlElement("application")]
+    public string Application { get; set; }
+
+    [XmlElement("messages")] 
+    public string FirstMessage { get; set; }
+
+}
